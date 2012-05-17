10ae1987aa3f43d7b02c959b707008fa1d2a7e32
Zac <zac@zac-Ubuntu.(none)>
2012-05-17 12:47:02 +0100

    Add method to create/add new user

--- pages/webdriver/sign_in.py
+++ pages/webdriver/sign_in.py
@@ -16,6 +16,8 @@ class SignIn(Base):
     _password_locator = (By.ID, 'password')
     _next_locator = (By.CSS_SELECTOR, 'button.start')
     _sign_in_locator = (By.CSS_SELECTOR, 'button.returning')
+    _verify_email_locator = (By.CSS_SELECTOR, 'button.newuser')
+    _content_header_locator = (By.CSS_SELECTOR, 'div.contents h2')
 
     def __init__(self, *args, **kwargs):
         Base.__init__(self, *args, **kwargs)
@@ -57,9 +59,23 @@ class SignIn(Base):
         self.selenium.find_element(*self._sign_in_locator).click()
         self.selenium.switch_to_window('')
 
+    def click_verify_email(self):
+        """Clicks verify email button and wait for success confirmation"""
+        self.selenium.find_element(*self._verify_email_locator).click()
+        WebDriverWait(self.selenium, self.timeout).until(
+            lambda s: s.find_element(*self._content_header_locator).text == 'Check your email!')
+
     def sign_in(self, email, password):
         """Signs in using the specified email address and password."""
         self.email = email
         self.click_next()
         self.password = password
         self.click_sign_in()
+
+    def verify_new_user(self, email):
+        """Enter an email address and click Verify Email button """
+        self.email = email
+        self.click_next()
+        self.click_verify_email()
+        self.selenium.close()
+        self.selenium.switch_to_window('')
