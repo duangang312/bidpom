8a8e85af93cea42ab8b82fd4df713cccd1f51951
Zac <zac@zac-Ubuntu.(none)>
2012-05-17 13:19:23 +0100

    add verify_new_user to browser_id.py

--- browser_id.py
+++ browser_id.py
@@ -12,12 +12,20 @@ class BrowserID(object):
     def __init__(self, selenium, timeout=60):
         self.selenium = selenium
         self.timeout = timeout
+        self._is_rc = isinstance(self.selenium, selenium.selenium)
 
     def sign_in(self, email, password):
         """Signs in using the specified email address and password."""
-        if isinstance(self.selenium, selenium.selenium):
+        if self._is_rc:
             from pages.rc.sign_in import SignIn
         else:
             from pages.webdriver.sign_in import SignIn
         sign_in = SignIn(self.selenium, timeout=self.timeout)
         sign_in.sign_in(email, password)
+
+    def verify_new_user(self, email):
+        """Add a new user via the verify new user workflow."""
+        if not self._is_rc:
+            from pages.webdriver.sign_in import SignIn
+            sign_in = SignIn(self.selenium, timeout=self.timeout)
+            sign_in.verify_new_user(email)
